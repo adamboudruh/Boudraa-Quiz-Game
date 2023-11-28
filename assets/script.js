1c34e53fb77593c76f4ac68798cc02cc36827534
Adam Boudraa <adamboudraa@gmail.com>
2023-11-28 12:07:27 -0800

    Added button functionality, click correct increases score, incorect decreases

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,40 @@
 var score = 0;
 var highScores = [];
 
-var beginButton = $('#begin');
+function refreshScore() {
+    $("#score").text("Score: " + score);
+}
 
+var correctOption = $('.correct');
+var incorrectOption = $('.incorrect');
+incorrectOption.on('click', function () {
+    score--;
+    refreshScore();
+});
+correctOption.on('click', function() {
+    score++;
+    refreshScore();
+});
+
+
+
+var beginButton = $('#begin');
 var questions = $('#questions').children();
 
 questions.on('hover', function() {
     questions.addClass('.active')
 });
 
-beginButton.on('click', function () {
+beginButton.on('click', firstQuestion);
 
 
+function firstQuestion() {
+    refreshScore();
+    $('#welcome-page').attr('style', 'display: none');
+    $('#question-container').attr('style', 'display: block');
+
+}
 
 
-});
 
 
