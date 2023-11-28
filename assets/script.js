981b3d101700278f18a161b9edd020c3f33779bd
Adam Boudraa <adamboudraa@gmail.com>
2023-11-28 11:48:14 -0800

    Added index.html, style.css, script.js

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var score = 0;
+var highScores = [];
+
+var beginButton = $('#begin');
+
+var questions = $('#questions').children();
+
+questions.on('hover', function() {
+    questions.addClass('.active')
+});
+
+beginButton.on('click', function () {
+
+
+
+
+});
+
+
