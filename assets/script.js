fb9c45fbab1e32ae28acde756e5c239c898772a6
Adam Boudraa <adamboudraa@gmail.com>
2023-11-29 02:22:21 -0800

    Tweaked questions

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,6 @@ var secondsLeft = 60;
 var anyOption = $('.option');
 var beginButton = $('#begin');
 var correctID = 3;
-var wasItCorrect = true;
-var questionAnswered = false;
 
 beginButton.on('click', function() {
     $('#questionTitle').attr('style', 'color: black');
@@ -53,7 +51,6 @@ $('#recordData').on('click', function(event){
     $('#scoreList').append(newScore);
     score = 0;
     questionNum = 0;
-    correctID = 3;
     $('#welcome-page').attr('style', 'display: block');
     $('#form-container').attr('style', 'display: none');
 })
@@ -64,11 +61,12 @@ function displayQuestion(questionNum) {
         $('#welcome-page').attr('style', 'display: none');
         $('#question-container').attr('style', 'display: block');
         console.log("first question");
-        $('#questionTitle').text("How old am I?");
-            $('#1').text("19 years old");
-            $('#2').text("18 years old");
-            $('#3').text("20 years old");
-            $('#4').text("22 years old");
+        $('#questionTitle').text("What is me and my wife's anniversary date?");
+            $('#1').text("November 21st");
+            $('#2').text("May 30th");
+            $('#3').text("December 29th");
+            $('#4').text("January 15th");
+            correctID = 3;
         $('body').attr('style', 'background-color: lightblue');
     }
     if(questionNum == 2) {
@@ -84,20 +82,20 @@ function displayQuestion(questionNum) {
     if(questionNum == 3) {
         console.log("third question");
         $('#questionTitle').text("When did Anakin lose his right arm?");
-            $('#1').text("In a fight against Obi wan");
+            $('#1').text("In the fight against Obi wan");
             $('#2').text("In the first fight against count dooku");
             $('#3').text("In the second fight against count dooku");
-            $('#4').text("In a fight against General Grievous");
+            $('#4').text("In the fight against General Grievous");
         correctID = 2;
         $('body').attr('style', 'background-color: lightseagreen');
     }
     if(questionNum == 4) {
         console.log("fourth and final question");
-        $('#questionTitle').text("Last One... What is my middle name?");
-            $('#1').text("Bryan");
-            $('#2').text("Myron");
-            $('#3').text("Byron");
-            $('#4').text("Ryan");
+        $('#questionTitle').text("Last One... What is my favorite pokemon?");
+            $('#1').text("Breloom");
+            $('#2').text("Oshawott");
+            $('#3').text("Krookodile");
+            $('#4').text("All of the above!");
         correctID = 4;
         $('body').attr('style', 'background-color: lightsalmon');
     }
